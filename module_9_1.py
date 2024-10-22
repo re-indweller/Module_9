e2915390b4cd94ce4f61a5b80c54abc9a396b743
re-indweller <asf.ayrat@gmail.com>
2024-10-22 21:34:54 +0300

    Add files via upload

---
+++ module_9_1.py
@@ -0,0 +1,11 @@
+# Домашнее задание по теме "Введение в функциональное программирование"
+# Задача "Вызов разом":
+def apply_all_func(int_list, *functions):
+    result = {}
+
+    for function in functions:
+        result[function.__name__] = function(int_list)
+    return result
+
+print(apply_all_func([6, 20, 15, 9], max, min))
+print(apply_all_func([6, 20, 15, 9], len, sum, sorted))
